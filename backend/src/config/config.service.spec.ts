b6e16c26a5b0b3ece9eca0322eb34a3755ebf8b7
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-01-23 15:01:57 +0700

    test(back->config): add config module test case

--- backend/src/config/config.service.spec.ts
+++ backend/src/config/config.service.spec.ts
@@ -3,14 +3,36 @@ import { ConfigService } from './config.service';
 
 describe('ConfigService', () => {
   let service: ConfigService;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ConfigService],
     }).compile();
     service = module.get<ConfigService>(ConfigService);
   });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return database username', () => {
+    expect(service.mySQLUser).toBeDefined();
+  });
+
+  it('should return database password', () => {
+    expect(service.mySQLPassword).toBeDefined();
+  });
+
+  it('should return database url', () => {
+    expect(service.mySQLServer).toBeDefined();
+  });
+
+  it('should return database name', () => {
+    expect(service.mySQLDatabaseName).toBeDefined();
+  });
+
 });
