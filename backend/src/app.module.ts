d81bd477a6f5139b59bb3adc9590186c76223641
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-01-23 15:11:55 +0700

    feat(back->user): add user module generated file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule],
+  imports: [ConfigModule, UserModule],
 })
 export class AppModule {}
