9abb803bb4587fd8929f7b10d12d9dcc631d7ff0
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-01-27 11:56:12 +0700

    feat(back->line): complete line token decoder module

---
+++ backend/src/line-token-decoder/dto/line-token.dto.ts
@@ -0,0 +1,10 @@
+export class LineToken {
+    iss: string;
+    sub: string;
+    aud: string;
+    exp: number;
+    iat: number;
+    name: string;
+    picture: string;
+    email: string;
+}
--- backend/src/line-token-decoder/line-token-decoder.service.ts
+++ backend/src/line-token-decoder/line-token-decoder.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LineToken } from './dto/line-token.dto';
 
 @Injectable()
-export class LineTokenDecoderService {}
+export class LineTokenDecoderService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    decode(token: string): LineToken {
+        try {
+            const result = this.jwtService.decode(token) as LineToken;
+            return result;
+        } catch (error) {
+            throw new UnauthorizedException(error);
+        }
+    }
+}
