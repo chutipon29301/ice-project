7b4419acec1faf2e2fb03077fa05a8f45bf98dae
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-01-30 10:04:31 +0700

    test(back->user): import user service to test case

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { DatabaseModule } from '../database.module';
+import { UserService } from './user.service';
+import { userProviders } from './user.providers';
 
 describe('User Controller', () => {
     let module: TestingModule;
+    let controller: UserController;
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
+            imports: [DatabaseModule],
             controllers: [UserController],
+            providers: [UserService, ...userProviders],
         }).compile();
-    });
-    it('should be defined', () => {
-        const controller: UserController = module.get<UserController>(
+        controller = module.get<UserController>(
             UserController,
         );
+    });
+
+    // beforeEach(async() => {
+    //     for(let i)
+    // });
+
+    // afterEach(async() => {
+
+    // });
+
+    it('should be defined', () => {
         expect(controller).toBeDefined();
     });
 });
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { DatabaseModule } from '../database.module';
+import { userProviders } from './user.providers';
 
 describe('UserService', () => {
     let service: UserService;
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UserService],
+            imports: [DatabaseModule],
+            providers: [UserService, ...userProviders],
         }).compile();
         service = module.get<UserService>(UserService);
     });
