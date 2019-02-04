a706c4baf1f24b438cde316a8ce51f6e2ba5320d
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-04 22:23:36 +0700

    feat(back->middleware): apply sanitizer and  helmet

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
@@ -8,6 +8,9 @@ import { LocationModule } from './location/location.module';
 import { LineModule } from './line/line.module';
 import { LineTokenDecoderModule } from './line-token-decoder/line-token-decoder.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { RoleModule } from './role/role.module';
+import { SanitizerMiddleware } from './middleware/sanitizer.middleware';
+import * as helmet from 'helmet';
 
 @Module({
     imports: [
@@ -18,8 +21,13 @@ import { CryptoModule } from './crypto/crypto.module';
         LineModule,
         LineTokenDecoderModule,
         CryptoModule,
+        RoleModule,
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(helmet(), SanitizerMiddleware).forRoutes('*');
+    }
+}
