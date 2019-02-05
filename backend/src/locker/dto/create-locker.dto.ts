837f25b7ea453a1b76d2ef512aa7d4a6e891dbf0
Sojirath <sojirath.thun@gmail.com>
2019-02-05 15:39:19 +0700

    docs(feat->locker): add docs for locker dto and controller

--- backend/src/locker/dto/create-locker.dto.ts
+++ backend/src/locker/dto/create-locker.dto.ts
@@ -1,13 +1,28 @@
 import { IsString, IsNumber } from 'class-validator';
 import { ToInt } from 'class-sanitizer';
+import { ApiModelProperty } from '@nestjs/swagger';
 export class CreateLockerDto {
+    @ApiModelProperty({
+      description: 'name of a locker determined and saw only by admin',
+      required: true,
+    })
     @IsString()
     name: string;
 
+
+    @ApiModelProperty({
+      description: 'location ID where the locker is',
+      required: true,
+    })
     @IsNumber()
     @ToInt()
     locationID: number;
 
+
+    @ApiModelProperty({
+      description: 'number of a locker in the location that is showed on the locker',
+      required: true,
+    })
     @IsString()
     number: string;
 }
--- backend/src/locker/locker.controller.ts
+++ backend/src/locker/locker.controller.ts
@@ -11,16 +11,20 @@ import { LockerService } from './locker.service';
 import Locker from '../models/Locker.model';
 import { CreateLockerDto } from './dto/create-locker.dto';
 import { EditLockerDto } from './dto/edit-locker.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 
 @Controller('locker')
 export class LockerController {
     constructor(private readonly lockerService: LockerService) {}
 
+    @ApiBadRequestResponse({description: 'cannot get list of lockers'})
     @Get()
     async listLocker(): Promise<Locker[]> {
         return await this.lockerService.list();
     }
 
+    @ApiCreatedResponse({description: 'new locker is created successfully'})
+    @ApiBadRequestResponse({description: 'cannot create locker'})
     @Post()
     async addLocker(@Body() createLockerDto: CreateLockerDto) {
         await this.lockerService.create(
@@ -30,11 +34,15 @@ export class LockerController {
         );
     }
 
+    @ApiOkResponse({description: 'editting data is succesful'})
+    @ApiBadRequestResponse({description: 'cannot edit data'})
     @Patch(':id')
     async edit(@Param('id') id: string, @Body() editLockerDto: EditLockerDto) {
         await this.lockerService.edit(parseInt(id, 10), editLockerDto);
     }
 
+    @ApiOkResponse({description: 'locker is deleted'})
+    @ApiBadRequestResponse({description: 'cannot delete the locker'})
     @Delete(':id')
     async delete(@Param('id') id: string) {
         await this.lockerService.delete(parseInt(id, 10));
