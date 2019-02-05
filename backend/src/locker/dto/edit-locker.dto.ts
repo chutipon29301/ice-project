f0ef453d7e38828a52f792ee0472f1919dd91134
Sojirath <sojirath.thun@gmail.com>
2019-02-05 15:39:19 +0700

    docs(feat->locker): add docs for locker dto and controller

--- backend/src/locker/dto/edit-locker.dto.ts
+++ backend/src/locker/dto/edit-locker.dto.ts
@@ -1,18 +1,35 @@
 import { IsString, IsNumber } from 'class-validator';
 import { ToInt } from 'class-sanitizer';
 import { LockerStatus } from 'src/models/Locker.model';
-import { DataType } from 'sequelize-typescript';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class EditLockerDto {
+    @ApiModelProperty({
+      description: 'name of a locker determined and seen only by admin',
+      required: false,
+    })
     @IsString()
     name: string;
 
+    @ApiModelProperty({
+      description: 'location ID where the locker is',
+      required: false,
+    })
     @IsNumber()
     @ToInt()
     locationID: number;
 
+    @ApiModelProperty({
+      description: 'number of a locker in the location that is showed on the locker',
+      required: false,
+    })
     @IsString()
     number: string;
 
+    @ApiModelProperty({
+      description: 'status of locker',
+      required: false,
+      enum: ['AVAILABLE', 'MAINTENANCE'],
+    })
     status: LockerStatus;
 }
