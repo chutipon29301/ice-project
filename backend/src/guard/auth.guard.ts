328aa9b1bf09595bf6d3650c1053daff6ededcdf
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-06 10:34:32 +0700

    feat(back->guard): implement auth guard

--- backend/src/guard/auth.guard.ts
+++ backend/src/guard/auth.guard.ts
@@ -1,14 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { LineAuthService } from '../line-auth/line-auth.service';
+import {Request} from 'express';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
+    }
+}
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly lineAuthService: LineAuthService) {}
+    constructor(private readonly lineAuthService: LineAuthService) { }
 
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
-        return true;
+        const request = context.switchToHttp().getRequest() as Request;
+        return request.user != null;
     }
 }
