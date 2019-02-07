27012dacdc106e2d9ced411d86324af95f4699a2
mingyanisa <mingignim@hotmail.com>
2019-02-07 20:26:47 +0700

    feat(back): add swagger to dto

--- backend/src/token/dto/token.dto.ts
+++ backend/src/token/dto/token.dto.ts
@@ -1,3 +1,9 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class Token {
+    @ApiModelProperty({
+        description: 'userID of registered user',
+        required: true,
+    })
     userID: number;
 }
