4483839cfcfb123c60455e7e261591b72a98c6e5
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-07 23:58:55 +0700

    fix(back->group): patch validation

--- backend/src/group/dto/edit-group.dto.ts
+++ backend/src/group/dto/edit-group.dto.ts
@@ -1,10 +1,12 @@
 import { ToInt } from 'class-sanitizer';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, IsOptional } from 'class-validator';
 
 export class EditGroupDto {
+    @IsOptional()
     @IsString()
     name: string;
 
+    @IsOptional()
     @IsNumber()
     @ToInt()
     maxHours: number;
