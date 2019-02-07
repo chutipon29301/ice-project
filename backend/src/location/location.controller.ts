3a28dab5edef6caa7d0a704ee3637569304a6773
mingyanisa <mingignim@hotmail.com>
2019-02-07 20:53:34 +0700

    feat(back->location): add swagger api response to location controller

--- backend/src/location/location.controller.ts
+++ backend/src/location/location.controller.ts
@@ -11,18 +11,25 @@ import { LocationService } from './location.service';
 import Location from '../models/location.model';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { EditLocationDto } from './dto/edit-location.dto';
-import { ApiUseTags } from '@nestjs/swagger';
+import {
+    ApiUseTags,
+    ApiBadRequestResponse,
+    ApiCreatedResponse,
+    ApiOkResponse,
+} from '@nestjs/swagger';
 
 @ApiUseTags('location')
 @Controller('location')
 export class LocationController {
     constructor(private readonly locationService: LocationService) {}
-
+    @ApiBadRequestResponse({ description: 'cannot get list of locations' })
     @Get()
     async listLocations(): Promise<Location[]> {
         return await this.locationService.list();
     }
 
+    @ApiCreatedResponse({ description: 'new location is created successfully' })
+    @ApiBadRequestResponse({ description: 'cannot create location' })
     @Post()
     addLocation(@Body() createLocationDto: CreateLocationDto) {
         return this.locationService.create(
@@ -31,6 +38,8 @@ export class LocationController {
         );
     }
 
+    @ApiOkResponse({ description: 'editing data is successful' })
+    @ApiBadRequestResponse({ description: 'cannot edit data' })
     @Patch('/:id')
     async edit(
         @Param('id') id: string,
@@ -39,6 +48,8 @@ export class LocationController {
         await this.locationService.edit(parseInt(id, 10), editLocationDto);
     }
 
+    @ApiOkResponse({ description: 'locker is deleted' })
+    @ApiBadRequestResponse({ description: 'cannot delete the locker' })
     @Delete(':id')
     async delete(@Param('id') id: string) {
         await this.locationService.delete(parseInt(id, 10));
