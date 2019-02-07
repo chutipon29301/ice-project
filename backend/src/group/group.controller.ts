a0cb601f130b458f2577724b72f5141cf6ec4f4b
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-08 00:01:23 +0700

    feat(back->group): add controller related to group service

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -1,22 +1,59 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Roles } from '../guard/role.decorator';
 import Group from '../models/group.model';
+import Users, { Role } from '../models/users.model';
 import { CreateGroupDto } from './dto/create-group.dto';
+import { EditGroupDto } from './dto/edit-group.dto';
+import { UserGroupDto } from './dto/user-group.dto';
 import { GroupService } from './group.service';
 
 @Controller('group')
 export class GroupController {
-    constructor(private readonly groupService: GroupService) {}
+    constructor(private readonly groupService: GroupService) { }
 
+    @Roles(Role.ADMIN)
     @Get()
-    async list(): Promise<Group[]> {
-        return await this.groupService.list();
+    async list(): Promise<{ groups: Group[] }> {
+        return { groups: await this.groupService.list() };
     }
 
+    @Roles(Role.ADMIN)
     @Post()
-    async create(@Body() createGroupDto: CreateGroupDto) {
-        await this.groupService.create(
-            createGroupDto.name,
-            createGroupDto.maxHours,
-        );
+    async create(@Body() body: CreateGroupDto) {
+        await this.groupService.create(body.name, body.maxHours);
+    }
+
+    @Roles(Role.ADMIN)
+    @Patch(':id')
+    async edit(@Param('id', new ParseIntPipe()) id: number, @Body() body: EditGroupDto) {
+        await this.groupService.edit(id, body);
+    }
+
+    @Roles(Role.ADMIN)
+    @Delete(':id')
+    async delete(@Param('id', new ParseIntPipe()) id: number) {
+        await this.groupService.delete(id);
+    }
+
+    @Roles(Role.ADMIN)
+    @Post('addUser')
+    async addUser(@Body() body: UserGroupDto) {
+        await this.groupService.add(body.userID, body.groupID);
+    }
+
+    @Roles(Role.ADMIN)
+    @Post('removeUser')
+    async removeUser(@Body() body: UserGroupDto) {
+        await this.groupService.remove(body.userID, body.groupID);
+    }
+
+    @Roles(Role.ADMIN)
+    @Get('detail/:id')
+    async detail(@Param('id', new ParseIntPipe()) id: number): Promise<{ group: Group, users: Users[] }> {
+        const group = await this.groupService.detailOfGroupID(id);
+        const users = await this.groupService.listUserInGroup(id);
+        return {
+            group, users,
+        };
     }
 }
