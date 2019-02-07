d5975728cc930588eb8a221141ea237bad0d353b
mingyanisa <mingignim@hotmail.com>
2019-02-07 20:26:47 +0700

    feat(back): add swagger to dto

--- backend/src/location/dto/edit-location.dto.ts
+++ backend/src/location/dto/edit-location.dto.ts
@@ -1,9 +1,18 @@
 import { IsString } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class EditLocationDto {
+    @ApiModelProperty({
+        description: 'name of the location that the locker is installed',
+        required: false,
+    })
     @IsString()
     name: string;
 
+    @ApiModelProperty({
+        description: 'detail of the location where the locker is',
+        required: false,
+    })
     @IsString()
     detail: string;
 }
--- backend/src/token/dto/token-object.dto.ts
+++ backend/src/token/dto/token-object.dto.ts
@@ -1,4 +1,15 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class TokenObject {
+    @ApiModelProperty({
+        description: 'generated token for permission user',
+        required: true,
+    })
     token: string;
+
+    @ApiModelProperty({
+        description: 'expire date for generated token',
+        required: true,
+    })
     expireDate: Date;
 }
