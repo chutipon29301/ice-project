6f4eb0b10b8ef90d93e92b76ac0f2e534fea8a9e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-08 11:51:50 +0700

    feat(back->locker): add relation on locker model

--- backend/src/models/locker.model.ts
+++ backend/src/models/locker.model.ts
@@ -7,30 +7,42 @@ import {
     Model,
     Table,
     BelongsTo,
+    AllowNull,
 } from 'sequelize-typescript';
 import Group from './group.model';
 import Location from './location.model';
 import LockerPermission from './locker-permission.model';
 import Users from './users.model';
-import UserPermission from './user-permission.model';
 import LockerOwner from './locker-owner.model';
 import LockerStat from './locker-stat.model';
 
 export enum LockerStatus {
+    UNREGISTERED = 'UNREGISTERED',
     AVAILABLE = 'AVAILABLE',
     MAINTENANCE = 'MAINTENANCE',
+    DELETE = 'DELETE',
 }
 
 @Table({
     timestamps: true,
 })
 export default class Locker extends Model<Locker> {
+    @AllowNull(true)
     @Column
     name: string;
 
+    @AllowNull(true)
     @ForeignKey(() => Location)
     @Column
     locationID: number;
+
+    @AllowNull(true)
+    @Column
+    number: string;
+
+    @Default(LockerStatus.AVAILABLE)
+    @Column(DataType.ENUM(Object.keys(LockerStatus)))
+    status: LockerStatus;
 
     @BelongsToMany(() => Group, () => LockerPermission)
     public groups: Group[];
@@ -40,11 +52,4 @@ export default class Locker extends Model<Locker> {
 
     @BelongsToMany(() => Users, () => LockerStat)
     public usedBy: Users[];
-
-    @Column
-    number: string;
-
-    @Default(LockerStatus.AVAILABLE)
-    @Column(DataType.ENUM(Object.keys(LockerStatus)))
-    status: LockerStatus;
 }
