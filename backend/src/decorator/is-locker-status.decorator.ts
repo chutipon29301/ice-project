514c330f74087f43e6f6fda3ff13261184e2e248
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-08 11:49:47 +0700

    feat(back->decorator): add is locker status array validator

---
+++ backend/src/decorator/is-locker-status.decorator.ts
@@ -0,0 +1,29 @@
+import {
+    registerDecorator,
+    ValidationOptions,
+    ValidationArguments,
+} from 'class-validator';
+import { LockerStatus } from '../models/locker.model';
+
+export function IsLockerStatus(
+    property?: string,
+    validationOptions?: ValidationOptions,
+) {
+    return (object: object, propertyName: string) => {
+        registerDecorator({
+            name: 'IsLockerStatus',
+            target: object.constructor,
+            propertyName,
+            constraints: [property],
+            options: validationOptions,
+            validator: {
+                validate(value: any[], args: ValidationArguments) {
+                    for (const status of value) {
+                        if (Object.keys(LockerStatus).indexOf(status) < 0) { return false; }
+                    }
+                    return true;
+                },
+            },
+        });
+    };
+}
