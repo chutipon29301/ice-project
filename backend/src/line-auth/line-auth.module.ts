f162bc16aaab6d70fa7221f70da62fddfe61bd4d
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-20 09:04:18 +0700

    feat(back->lineAuth): add decode function

--- backend/src/line-auth/line-auth.module.ts
+++ backend/src/line-auth/line-auth.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { LineAuthService } from './line-auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { CryptoModule } from 'src/crypto/crypto.module';
 
 @Module({
+    imports: [JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+            secretOrPrivateKey: configService.lineChannelSecret,
+            signOptions: {
+                algorithm: 'HS256',
+                audience: configService.lineChannelID,
+                issuer: 'https://access.line.me',
+            },
+        })
+    }), ConfigModule, HttpModule, CryptoModule],
     providers: [LineAuthService],
+    exports: [LineAuthService]
 })
-export class LineAuthModule {}
+export class LineAuthModule { }
