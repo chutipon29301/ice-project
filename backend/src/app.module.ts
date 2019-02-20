4cf9ad76cf3dbba07eee37445c7a5b86eddeec8b
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-20 09:03:22 +0700

    feat(back->crypto): add crypto module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { LineAuthModule } from './line-auth/line-auth.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-    imports: [ConfigModule, LineAuthModule],
+    imports: [ConfigModule, LineAuthModule, CryptoModule],
     controllers: [AppController],
     providers: [AppService],
 })
