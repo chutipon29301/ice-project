6620c7464d748837fb3a7a70d4764291ef9f1641
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-02-20 15:53:41 +0700

    feat(back->auth): line auth link

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Get('/line')
+    public async lineAuth(@Res() res: Response) {
+        res.redirect(this.authService.getLineAuthenticationPageURL());
+    }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LineAuthService } from '../line-auth/line-auth.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    private readonly lineCallbackURL: string = '/auth/line/callback';
+
+    constructor(private readonly lineAuthService: LineAuthService) {}
+
+    getLineAuthenticationPageURL(): string {
+        return this.lineAuthService.lineAuthPageURL(this.lineCallbackURL);
+    }
+}
