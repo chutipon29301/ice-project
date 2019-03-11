536db421a44b4459c994cf4a034dc392a9f136ca
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-03-11 11:34:15 +0700

    feat(back->auth): line auth callback

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Query, UnauthorizedException, Post } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
-    @Get('/line')
+    @Get('line')
     public async lineAuth(@Res() res: Response) {
         res.redirect(this.authService.getLineAuthenticationPageURL());
+    }
+
+    @Get('line/callback')
+    async lineCallback(
+        @Query('code') code: string,
+        @Query('state') state: string,
+        @Query('error') error: string,
+        @Query('errorCode') errorCode: string,
+        @Query('errorMessage') errorMessage: string,
+        // @Res() res: Response,
+    ): Promise<string> {
+        if (error != null || errorCode != null || errorMessage != null) {
+            throw new UnauthorizedException(errorMessage);
+        }
+        if (await this.authService.validateState(code, state)) {
+            return code;
+        }
     }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LineAuthService } from '../line-auth/line-auth.service';
+import { LineToken } from 'src/line-auth/dto/line-token.dto';
+import { JwtService } from '@nestjs/jwt';
+import { State } from 'src/line-auth/dto/state.dto';
+import { CryptoService } from 'src/crypto/crypto.service';
+import { ConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class AuthService {
     private readonly lineCallbackURL: string = '/auth/line/callback';
-
-    constructor(private readonly lineAuthService: LineAuthService) {}
+    private readonly passPhase = 'Hello World!';
+    constructor(private readonly lineAuthService: LineAuthService,
+                private readonly cryptoService: CryptoService,
+                private readonly configService: ConfigService) {}
 
     getLineAuthenticationPageURL(): string {
         return this.lineAuthService.lineAuthPageURL(this.lineCallbackURL);
     }
+
+    async validateState(
+        code: string,
+        encryptedState: string,
+    ): Promise<boolean> {
+        const state = State.from(
+            this.cryptoService.AES.decrypt(
+                decodeURIComponent(encryptedState),
+                this.configService.lineChannelSecret,
+            ),
+        );
+        if (!state.compareTo(this.passPhase)) {
+            throw new UnauthorizedException(
+                'Line authenticate wrong callback state',
+            );
+        } else {
+            return true;
+        }
+    }
+
 }
