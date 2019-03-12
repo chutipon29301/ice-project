9a68086e93526adfb153893c711e786b9a4936bc
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-03-12 23:29:59 +0700

    feat(back->locker): add weak relation between locker and locker instance

--- backend/src/entities/locker-instance.entity.ts
+++ backend/src/entities/locker-instance.entity.ts
@@ -1,15 +1,27 @@
-import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn, PrimaryColumn, RelationId } from 'typeorm';
 import { Locker } from './locker.entity';
 
 @Entity()
 export class LockerInstance {
+    constructor(
+        date: Date,
+        locker: Locker,
+    ) {
+        this.date = date;
+        this.locker = locker;
+    }
 
-    @Column()
-    inUsed: boolean;
-
-    @Column()
+    @PrimaryColumn()
     date: Date;
 
-    @ManyToOne(type => Locker)
+    @Column({
+        type: 'boolean',
+        default: true
+    })
+    inUsed: boolean;
+
+    @ManyToOne(type => Locker, { primary: true })
+    @JoinColumn({ referencedColumnName: 'id' })
     locker: Locker;
+
 }
