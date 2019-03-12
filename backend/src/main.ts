fadccd6c1c863dd9d4ab2c4dd3a098f404e3c4c1
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-03-12 14:28:48 +0700

    fix(back->pipe): global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(new ValidationPipe());
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
 }
 bootstrap();
