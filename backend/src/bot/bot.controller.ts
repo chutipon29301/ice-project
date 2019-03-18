e9aabd259a5db864c4ed3f9f70a499f80e62522e
Sojirath <sojirath.thun@gmail.com>
2019-03-18 11:28:20 +0700

    feat(back->bot): edit get photo from bot
    # Conflicts:
    #	backend/src/app.module.ts

--- backend/src/bot/bot.controller.ts
+++ backend/src/bot/bot.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Res, Param } from '@nestjs/common';
+import { Controller, Get, Res, Param, Post, Body, Req } from '@nestjs/common';
 import { NestApplication } from '@nestjs/core';
+import { BotService } from './bot.service';
+
 const path = require('path');
+
 @Controller('bot')
 export class BotController {
+  constructor (private readonly service:BotService){}
 
-  @Get(':fileName')
+  @Post('replyline')
+  async replyMsg(@Req()req){
+    return await this.service.lineBotReplyMsg(req);
+  }
+  @Get('getPhoto/:fileName')
   async list(@Res() res, @Param('fileName') fileName: string) {
       return await res.sendFile(path.join(__dirname, './', fileName));
-
   }
+
+
 }
