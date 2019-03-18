4afc7825514adac4dd6bbe983a31e1ddf05a469f
Nicha <NichaRoj@gmail.com>
2019-03-18 14:10:30 +0700

    ci: Edit config and add a script file

---
+++ run-docker-compose-up.sh
@@ -0,0 +1,7 @@
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:/$PWD" \
+    -w="/$PWD" \
+    docker/compose:1.23.2 \
+    -f docker-compose.yml -f docker-compose.prod.yml \
+    up --force-recreate -d
