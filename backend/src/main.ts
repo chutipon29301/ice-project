057858190340b70e369322aaf0572a0c4a1a455f
mingyanisa <mingignim@hotmail.com>
2019-03-19 14:04:52 +0700

    feat(back->swagger): add swagger in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    const options = new DocumentBuilder()
+        .setTitle('Locker swarm example')
+        .setDescription('The locker swarm API description')
+        .setVersion('1.0')
+        .addTag('locker-swarm')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();
