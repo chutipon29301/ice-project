c32f4e632ac215f6fe0359a1c8e90139df26c89b
BB-8 <ichannel.jakpat@gmail.com>
2019-03-20 14:22:38 +0700

    feat(back): add ToDateFromTimestamp sanitizer
    ToDateFromTimestamp is a custom sanitizer that sanitize timestamp to Date

---
+++ backend/src/sanitizer.ts
@@ -0,0 +1,25 @@
+import {
+    SanitizerInterface,
+    SanitizerConstraint,
+    Sanitize,
+} from 'class-sanitizer';
+
+export const ToDateFromTimestamp = () => Sanitize(ToDateFromTimestampSanitizer);
+
+@SanitizerConstraint()
+class ToDateFromTimestampSanitizer implements SanitizerInterface {
+    sanitize(text: string): Date | null {
+        const val = parseInt(text, 10);
+        if (isNaN(val)) {
+            // throw new BadRequestException('Validation failed');
+            return null;
+        }
+        const timestamp = val * 1000;
+        const date = new Date(timestamp);
+        return isValidDate(date) ? date : null;
+    }
+}
+
+function isValidDate(date: any): date is Date {
+    return date instanceof Date && !isNaN(date as any);
+}
