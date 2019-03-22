8218d058a523ed87a12d8547a507ca9534cdab58
Sojirath <sojirath.thun@gmail.com>
2019-03-23 01:15:51 +0700

    feat(back->bot): add find locker in helpme reply bot

--- backend/src/line-response/help-me.response.ts
+++ backend/src/line-response/help-me.response.ts
@@ -1,39 +1,55 @@
-export const helpMeResponse = [
-  {
-    type: "template",
-    altText: "this is a carousel template",
-    template: {
-        type: "carousel",
-        columns: [
-            {
-              thumbnailImageUrl: "https://1c927224.ngrok.io/bot/replyImage/general-problem.jpg?_ignored=",
+export const helpMeResponse = function (serverURL:string) {
+  return  [
+   {
+     type: "template",
+     altText: "this is a carousel template",
+     template: {
+         type: "carousel",
+         columns: [
+             {
+               thumbnailImageUrl: `${serverURL}/bot/replyImage/howtouse-banner.jpg?_ignored=`,
+               imageBackgroundColor: "#FFFFFF",
+               title: "How to use 🔧📘",
+               text: "Don't know how to use LockerSwam? Click here!",
+               actions: [
+                   {
+                     type: 'postback',
+                     label: 'Get manual',
+                     data: 'manual',
+                   }
+               ]
+             },
+             {
+              thumbnailImageUrl: `${serverURL}/bot/replyImage/findlocker-banner.jpg?_ignored=`,
               imageBackgroundColor: "#FFFFFF",
-              title: "General problem",
-              text: "Have a problem? Click here!",
+              title: "Can't find the locker ❓",
+              text: "Find available lockers",
               actions: [
                   {
-                    type: 'postback',
-                    label: 'Get manual',
-                    data: 'manual',
+                    type: 'uri',
+                    label: 'Find',
+                    uri: 'line://app/1643085874-xeWvgKgE',
                   }
               ]
             },
-            {
-              thumbnailImageUrl: "https://1c927224.ngrok.io/bot/replyImage/report.jpg?_ignored=",
-              imageBackgroundColor: "#000000",
-              title: "Faulty locker",
-              text: "Unable",
-              actions: [
-                  {
-                      type: "postback",
-                      label: "Report",
-                      data: "report"
-                  },
-              ]
-            }
-        ],
-        imageAspectRatio: "rectangle",
-        imageSize: "cover"
-    }
-  }
-];
+             {
+               thumbnailImageUrl: `${serverURL}/bot/replyImage/report.jpg?_ignored=`,
+               imageBackgroundColor: "#000000",
+               title: `Faulty locker ❓`,
+               text: `Unable to use locker            or any emergencies`,
+               actions: [
+                   {
+                       type: "postback",
+                       label: "Report",
+                       data: "report"
+                   },
+               ]
+             }
+         ],
+         imageAspectRatio: "rectangle",
+         imageSize: "cover"
+     }
+   }
+
+ ];
+}
