60de2cbdabfdc731895f01ea3fd32e86fdff9e12
Sojirath <sojirath.thun@gmail.com>
2019-03-23 01:12:27 +0700

    feat(back->bot): edit bot reply to prevent callback on first greeting

--- backend/src/bot/bot.service.ts
+++ backend/src/bot/bot.service.ts
@@ -1,10 +1,11 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { defaultResponse } from 'src/line-response/default.reponse';
+import { manualResponse } from 'src/line-response/manual.response';
+import { reportResponse } from 'src/line-response/report.response';
 import { ConfigService } from '../config/config.service';
 import { helpMeResponse } from '../line-response/help-me.response';
 import { LineUserEventDto } from './dto/line-user-event.dto';
-import { manualResponse } from 'src/line-response/manual.response';
-import { reportResponse } from 'src/line-response/report.response';
-import { defaultResponse } from 'src/line-response/default.reponse';
+import { findLockerResponse } from 'src/line-response/findLocker.response';
 
 @Injectable()
 export class BotService {
@@ -14,27 +15,33 @@ export class BotService {
     ) {}
 
     async lineBotReplyMsg(body: LineUserEventDto) {
-
         const event = body.events[0];
-        let msg = "";
-        switch(event.type){
-          case 'message':
-            msg = event.message.text;
-            break;
-          case 'postback':
-            msg = event.postback.data;
-            break;
+
+        if(event.type === 'follow'){
+          return;
         }
-        console.log(msg);
+        let msg = "";
+          switch(event.type){
+            case 'message':
+              msg = event.message.text;
+              break;
+            case 'postback':
+              msg = event.postback.data;
+              break;
+          }
+
         switch (msg) {
             case 'help':
-                await this.reply(event.replyToken, helpMeResponse);
+                await this.reply(event.replyToken, helpMeResponse(this.configService.serverURL));
                 break;
             case 'manual':
-                await this.reply(event.replyToken, manualResponse);
+                await this.reply(event.replyToken, manualResponse(this.configService.serverURL));
                 break;
             case 'report':
                 await this.reply(event.replyToken, reportResponse);
+                break;
+            case 'find locker':
+                await this.reply(event.replyToken, findLockerResponse);
                 break;
             default:
                 await this.reply(event.replyToken, defaultResponse);
