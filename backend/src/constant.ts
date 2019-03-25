659581cf29d9f5cb3ffc6c5e560861077ecbd35f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-03-26 00:11:14 +0700

    feat(back->qr): add qr code repository constant

--- backend/src/constant.ts
+++ backend/src/constant.ts
@@ -5,3 +5,4 @@ export const LockerRepositoryToken = 'LockerRepositoryToken';
 export const LockerInstanceRepositoryToken = 'LockerInstanceRepositoryToken';
 export const LockerUsageRepositoryToken = 'LockerUsageRepositoryToken';
 export const GroupRepositoryToken = 'GroupRepositoryToken';
+export const QRCodeRepositoryToken = 'QRCodeRepositoryToken';
---
+++ backend/src/qr/qr.providers.ts
@@ -0,0 +1,10 @@
+import { Provider } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { DbConnectionToken, QRCodeRepositoryToken } from '../constant';
+import { QRCode } from '../entities/qr-code.entity';
+
+export const qrProviders: Provider[] = [{
+    provide: QRCodeRepositoryToken,
+    useFactory: (connection: Connection) => connection.getRepository(QRCode),
+    inject: [DbConnectionToken],
+}];
