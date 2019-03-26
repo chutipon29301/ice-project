6e1a8d9f3ff66185a067482644a5761d87c5994a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-03-26 22:33:12 +0700

    feat(back->decorator): add type when decorator return value

--- backend/src/decorator/user.decorator.ts
+++ backend/src/decorator/user.decorator.ts
@@ -1,5 +1,6 @@
 import { createParamDecorator } from '@nestjs/common';
+import { JwtToken } from '../jwt-auth/dto/jwt-token.dto';
 
-export const User = createParamDecorator((data, req) => {
+export const User = createParamDecorator((data, req): JwtToken => {
     return req.user;
 });
