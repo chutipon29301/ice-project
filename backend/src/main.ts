ba0489f8605318d7236f6981a36ee116289b35a8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-01 22:29:40 +0700

    feat(backend): enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,8 @@ import { join } from 'path';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
+
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalPipes(new SanitizationPipe());
     app.useStaticAssets(join(__dirname, '../public'));
