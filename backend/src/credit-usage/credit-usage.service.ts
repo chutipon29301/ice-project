210f9e86c2cb673d5efd2cda0e0470575f992420
mingyanisa <mingignim@hotmail.com>
2019-04-05 16:06:39 +0700

    feat(back->creditUsage): add get myCredit and inject repo

--- backend/src/credit-usage/credit-usage.service.ts
+++ backend/src/credit-usage/credit-usage.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { CreditUsageRepositoryToken } from 'src/constant';
+import { Repository } from 'typeorm';
+import { CreditUsage } from 'src/entities/credit-usage.entity';
 
 @Injectable()
-export class CreditUsageService {}
+export class CreditUsageService {
+    constructor(
+        @Inject(CreditUsageRepositoryToken)
+        private readonly creditUsageRepository: Repository<CreditUsage>,
+    ) { }
+    public async getMyCredit(nationalID: string) {
+        await this.creditUsageRepository.findOneOrFail({where:nationalID});
+        const {totalCredit} = await this.creditUsageRepository
+        .createQueryBuilder('credit')
+        .select('SUM(credit.nationalID)', 'sum')
+        .where("credit.nationalID = :nationalID", { id: nationalID })
+        .getRawOne();
+        console.log({totalCredit})
+        return {totalCredit};
+    }
+    public async addCredit(amount:number){
+
+    }
+
+}
