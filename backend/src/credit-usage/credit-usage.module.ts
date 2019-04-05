b0404647c3051ff4ba33be30b4558291f5ce5db1
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-05 22:06:52 +0700

    fix(back->credit): import missing module

--- backend/src/credit-usage/credit-usage.module.ts
+++ backend/src/credit-usage/credit-usage.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { CreditUsageService } from './credit-usage.service';
 import { CreditUsageController } from './credit-usage.controller';
 import { creditUsageProviders } from './credit-usage.provider';
+import { UserModule } from '../user/user.module';
 
 @Module({
+    imports: [UserModule],
     providers: [
         CreditUsageService,
         ...creditUsageProviders,
     ],
     controllers: [CreditUsageController],
 })
-export class CreditUsageModule {}
+export class CreditUsageModule { }
