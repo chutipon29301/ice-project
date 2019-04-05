b36aef32c095c4fda7e8d38e1dbc0a099c8f3658
mingyanisa <mingignim@hotmail.com>
2019-04-05 20:45:24 +0700

    feat(back->creditUsage): add user credit to db

--- backend/src/credit-usage/credit-usage.service.ts
+++ backend/src/credit-usage/credit-usage.service.ts
@@ -2,25 +2,29 @@ import { Injectable, Inject } from '@nestjs/common';
 import { CreditUsageRepositoryToken } from 'src/constant';
 import { Repository } from 'typeorm';
 import { CreditUsage } from 'src/entities/credit-usage.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class CreditUsageService {
     constructor(
         @Inject(CreditUsageRepositoryToken)
         private readonly creditUsageRepository: Repository<CreditUsage>,
+        private readonly userService: UserService,
     ) { }
     public async getMyCredit(nationalID: string) {
-        await this.creditUsageRepository.findOneOrFail({where:nationalID});
-        const {totalCredit} = await this.creditUsageRepository
-        .createQueryBuilder('credit')
-        .select('SUM(credit.nationalID)', 'sum')
-        .where("credit.nationalID = :nationalID", { id: nationalID })
-        .getRawOne();
-        console.log({totalCredit})
-        return {totalCredit};
+        await this.creditUsageRepository.findOneOrFail({ where: nationalID });
+        const { totalCredit } = await this.creditUsageRepository
+            .createQueryBuilder('credit')
+            .select('SUM(credit.nationalID)', 'sum')
+            .where("credit.nationalID = :nationalID", { id: nationalID })
+            .getRawOne();
+        console.log({ totalCredit })
+        return { totalCredit };
     }
-    public async addCredit(amount:number){
-
+    public async addCredit(amount: number, nationalID: string) {
+        const user = await this.userService.findUserWithNationalIDOrFail(nationalID);
+        const creditUse = new CreditUsage(amount, user);
+        this.creditUsageRepository.save(creditUse);
     }
 
 }
