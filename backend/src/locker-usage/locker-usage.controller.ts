2a338ca0967f172f2c7d651c459472374510e0c5
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-05 09:48:23 +0700

    refactor(back->usage): remove locker usage controller

--- backend/src/locker-usage/locker-usage.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('locker-usage')
-export class LockerUsageController {}
--- backend/src/locker-usage/locker-usage.module.ts
+++ backend/src/locker-usage/locker-usage.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { LockerUsageController } from './locker-usage.controller';
 import { lockerUsageProviders } from './locker-usage.providers';
 import { LockerUsageService } from './locker-usage.service';
 
 @Module({
     providers: [...lockerUsageProviders, LockerUsageService],
-    controllers: [LockerUsageController],
     exports: [LockerUsageService],
 })
 export class LockerUsageModule {}
