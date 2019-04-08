6f59cb6bb467715f40179f77837f9a8c79fa679a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-08 23:01:17 +0700

    feat(back->credit): shoe credit history

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -5,18 +5,33 @@ import { CreateAdminEntityDto } from './dto/create-admin-entity.dto';
 import { User, Role } from '../entities/user.entity';
 import { Roles } from '../guard/role.decorator';
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
+import { User as UserDecorator } from '../decorator/user.decorator'
+import { JwtToken } from '../jwt-auth/dto/jwt-token.dto';
 
 @ApiUseTags('User')
 @ApiBearerAuth()
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Roles(Role.SUPERUSER, Role.ADMIN)
     @Get()
     async list(): Promise<{ users: User[] }> {
         const users = await this.userService.listUser();
         return { users };
+    }
+
+    @Roles(Role.USER, Role.SUPERUSER)
+    @Get('creditHistory')
+    async creditHistory(
+        @UserDecorator() user: JwtToken,
+    ): Promise<User> {
+        return this.userService.findUser({
+            key: {
+                nationalID: user.nationalID
+            },
+            joinWith: ['creditUsages'],
+        });
     }
 
     @Post('register')
