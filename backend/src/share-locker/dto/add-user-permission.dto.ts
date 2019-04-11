e232e348ca6e149e725c216a7bb60c4b93d04c61
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-12 00:23:10 +0700

    docs(back->share): add api documentation to share locker dto

--- backend/src/share-locker/dto/add-user-permission.dto.ts
+++ backend/src/share-locker/dto/add-user-permission.dto.ts
@@ -1,3 +1,9 @@
+import { ApiModelProperty } from "@nestjs/swagger";
+
 export class AddUserPermissionDto {
+    @ApiModelProperty({
+        description: 'locker access code that is given to user',
+        required: true,
+    })
     accessCode: string;
 }
--- backend/src/share-locker/dto/revoke-permission.dto.ts
+++ backend/src/share-locker/dto/revoke-permission.dto.ts
@@ -1,11 +1,20 @@
 import { ToInt } from 'class-sanitizer';
 import { IsNumber, IsString } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class RevokePermissionDto{
+    @ApiModelProperty({
+        description: 'locker ID that user wants to revoke some of shared permission of',
+        required: true,
+    })
     @IsNumber()
     @ToInt()
     lockerID: number;
 
+    @ApiModelProperty({
+        description: 'amount of money user wants to top up',
+        required: true,
+    })
     @IsString()
     nationalID: string;
 }
