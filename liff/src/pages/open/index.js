6e237bd3af3802377414562f44cfef0833610c96
Tai <friendship045@gmail.com>
2019-04-16 20:15:25 +0700

    feat: sync open-locker page with backend

--- liff/src/pages/open/index.js
+++ liff/src/pages/open/index.js
@@ -3,13 +3,21 @@ import QrReader from "react-qr-reader";
 import { Modal } from "antd";
 import { NavBar } from "antd-mobile";
 import Axios from "axios";
-
+import { liffHelper } from "../../App";
 const Deposit = () => {
   const [{ result, delay }, setQRCode] = useState({ result: "", delay: 300 });
   const [showModal, setModal] = useState(false);
+  const [accessCode, setAccessCode] = useState("");
 
   useEffect(() => {
     document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+    const location = window.location.href;
+    const indexOfEqual = location.indexOf("=");
+    if (indexOfEqual !== -1) {
+      const accessCode = location.substring(indexOfEqual + 1);
+      setAccessCode(accessCode);
+    }
+
     return function cleanup() {
       document.body.style.backgroundColor = "";
     };
@@ -17,9 +25,42 @@ const Deposit = () => {
 
   const afterScan = function(data) {
     if (data === null) return;
+    if (accessCode === "") {
+      const indexOfEqual = data.indexOf("=");
+      const achievedAccessCode = data.substring(indexOfEqual + 1);
+      setAccessCode(achievedAccessCode);
+    }
     setQRCode({ result: data, delay: 300 });
     setModal(true);
   };
+
+  const openLocker = async () => {
+    const {
+      data: { isInUsed }
+    } = await Axios.get(`/locker-instance/isInUsed?accessCode=${accessCode}`);
+    if (isInUsed) {
+      try {
+        await Axios.post(`/locker-instance/unlock`, {
+          accessCode: accessCode
+        });
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    } else {
+      try {
+        await Axios.post(`/locker-instance/createInstance`, {
+          accessCode: accessCode
+        });
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    }
+    setModal(false);
+    liffHelper.closeLiff();
+  };
+  console.log(accessCode, "accessCode");
   return (
     <div>
       <NavBar mode="dark">Open Locker</NavBar>
@@ -30,12 +71,7 @@ const Deposit = () => {
         onCancel={() => {
           setModal(false);
         }}
-        onOk={() => {
-          Axios.post("/open-locker").then(() => {
-            setModal(false);
-            window.location.replace("https://line.me/R/ti/p/f4TRdbATay");
-          });
-        }}
+        onOk={openLocker}
       >
         {result}
       </Modal>
