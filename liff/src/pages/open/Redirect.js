81b9d6c1a830efdbb5cfeff15548663a7fa8b654
Tai <friendship045@gmail.com>
2019-04-16 20:15:25 +0700

    feat: sync open-locker page with backend

--- liff/src/pages/open/Redirect.js
+++ liff/src/pages/open/Redirect.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { liffHelper } from "../../App";
+import { Redirect as ReactRedirect } from "react-router-dom";
+
 const Redirect = ({ liffData }) => {
-  if (liffData === null) return <div> This should be redirect page lmao</div>;
+  if (liffData === null) return <ReactRedirect to="/open-locker" />;
   else {
     liffHelper.openExternal(
       "https://ice-project-liff.herokuapp.com/open-locker",
