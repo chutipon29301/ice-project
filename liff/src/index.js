f18e439d32b1488d9d9d00857bc559a34c4adf9b
Tai <friendship045@gmail.com>
2019-04-30 18:20:43 +0700

    config(front->liff): hard code backend endpoint URL

--- liff/src/index.js
+++ liff/src/index.js
@@ -7,7 +7,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import Axios from "axios";
 
-export const END_POINT = REACT_APP_PRODUCTION_ENDPOINT;
+export const END_POINT = "https://api.lockerswarm.xyz";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   END_POINT = "https://10e2f066.ngrok.io";
 // } else {
